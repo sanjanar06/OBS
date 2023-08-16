13b0dc0a2c810e1a203fb885a59af2c32f146407
maniamanish <manish98gautam@gmail.com>
2023-08-16 15:08:50 +0530

    change password page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import AccountDetails from './pages/UserDashboard/AccountDetails.jsx';
 import AccountSummary from './pages/UserDashboard/AccountSummary.jsx';
 import ChangeUid_pass from './pages/Home/ForgotPassword.jsx';
 import UserProfile from './pages/UserDashboard/UserProfile.jsx';
+import ChangePassword from './pages/changePassword';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/AccountSummary" element={<AccountSummary/>}/> 
           <Route path="/Change" element={<ChangeUid_pass/>}/> 
           <Route path="/UserProfile" element={<UserProfile/>}/> 
+          <Route path="/ChangePassword" element = {<ChangePassword/>} />
       </Routes>
 
     </div>
