5136edc534a71d5d1e9996322d6c02882aefba54
anshstar55 <anshul.a4571@gmail.com>
2023-08-21 13:30:09 +0530

    IMPS payment page changed to function and fake jason testing integrated

--- frontend/src/pages/FundTransfer/impsPayment.jsx
+++ frontend/src/pages/FundTransfer/impsPayment.jsx
@@ -1,134 +1,136 @@
-import React, { Component } from 'react';
-import '../style/IMPSPayment.css'; // You can import your CSS file here for styling
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../style/IMPSPayment.css'; // You can import your CSS file here for styling
+import axios from 'axios';
 
-class IMPSPayment extends Component {
-  constructor(props) {
-    super(props);
+function IMPSPayment() {
+  const [formData, setFormData] = useState({
+    fromAccount: '',
+    toAccount: '',
+    amount: '',
+    date: '',
+    maturityInstruction: '',
+    remarks: '',
+  });
 
-    this.state = {
+  const handleInputChange = (field, value) => {
+    setFormData({
+      ...formData,
+      [field]: value,
+    });
+  };
+
+  const handleSaveClick = async() => {
+    try {
+      const response = await axios.post('http://localhost:3001/imps', formData);
+      console.log('IMPS Payment saved:', response.data);
+      alert("Data has been saved succesfully");
+      
+    } catch (error) {
+      console.error('Error saving transaction:', error);
+      
+    }
+  }
+
+
+  const handleReset = () => {
+    setFormData({
       fromAccount: '',
       toAccount: '',
       amount: '',
       date: '',
       maturityInstruction: '',
       remarks: '',
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
-
-  handleInputChange(e) {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
     });
-  }
+  };
 
-  handleSubmit(e) {
-    e.preventDefault();
-    // Here, you can perform any actions with the form data, such as initiating IMPS payment.
-    console.log(this.state);
-  }
-
-  handleReset() {
-    this.setState({
-      fromAccount: '',
-      toAccount: '',
-      amount: '',
-      date: '',
-      maturityInstruction: '',
-      remarks: '',
-    });
-  }
-
-  render() {
-    return (
-      <div>
-          <h1>Initiate IMPS Payment</h1>
-          <form className="imps-form" onSubmit={this.handleSubmit}>
-            <div1>
-            <label>From Account:</label>
-            <input
-              type="text"
-              name="fromAccount"
-              required
-              value={this.state.fromAccount}
-              onChange={this.handleInputChange}
-            />
-            </div1>
-            
-            <div1>
-            <label>To Account:</label>
-            <input
-              type="text"
-              name="toAccount"
-              required
-              value={this.state.toAccount}
-              onChange={this.handleInputChange}
-            />
-            <Link to="/addbeneficiary">
-            <button type="button" className="add-new-button">Add New +</button>
-            </Link>
-            </div1>
-
-            <div1>
-            <label>Amount:</label>
-            <input
-              type="text"
-              name="amount"
-              required
-              value={this.state.amount}
-              onChange={this.handleInputChange}
-            />
-            </div1>
-
-            <div1>
-            <label>Date:</label>
-            <input
-              type="date"
-              name="date"
-              required
-              value={this.state.date}
-              onChange={this.handleInputChange}
-            />
-            </div1>
-
-            <div1>
-            <label>Maturity Instruction:</label>
-            <input
-              type="text"
-              name="maturityInstruction"
-              required
-              value={this.state.maturityInstruction}
-              onChange={this.handleInputChange}
-            />
-            </div1>
-
-            <div1>
-            <label>Remarks:</label>
-            <input
-              type="text"
-              name="remarks"
-              value={this.state.remarks}
-              onChange={this.handleInputChange}
-            />
-            </div1>
-            
-            <div1>
-            <div className="buttons">
-              <button type="submit">Continue</button>
-              <button type="button" onClick={this.handleReset}>Reset</button>
-              <button type="button">Save</button>
-              <button type="button">Save as Template</button>
-            </div>
-            </div1>
-          </form>
+  return (
+    <div>
+      <h1>Initiate IMPS Payment</h1>
+      <form className="imps-form">
+        <div className="form-group">
+          <label>From Account:</label>
+          <input
+            type="text"
+            name="fromAccount"
+            required
+            value={formData.fromAccount}
+            onChange={e => handleInputChange('fromAccount', e.target.value)} 
+          />
         </div>
-    );
-  }
+
+        <div className="form-group">
+          <label>To Account:</label>
+          <input
+            type="text"
+            name="toAccount"
+            required
+            value={formData.toAccount}
+            onChange={e => handleInputChange('toAccount', e.target.value)} 
+          />
+          <Link to="/addbeneficiary">
+            <button type="button" className="add-new-button">
+              Add New +
+            </button>
+          </Link>
+        </div>
+
+        <div className="form-group">
+          <label>Amount:</label>
+          <input
+            type="text"
+            name="amount"
+            required
+            value={formData.amount}
+            onChange={e => handleInputChange('amount', e.target.value)} 
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Date:</label>
+          <input
+            type="date"
+            name="date"
+            required
+            value={formData.date}
+            onChange={e => handleInputChange('date', e.target.value)} 
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Maturity Instruction:</label>
+          <input
+            type="text"
+            name="maturityInstruction"
+            required
+            value={formData.maturityInstruction}
+            onChange={e => handleInputChange('maturityInstruction', e.target.value)} 
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Remarks:</label>
+          <input
+            type="text"
+            name="remarks"
+            value={formData.remarks}
+            onChange={e => handleInputChange('remarks', e.target.value)} 
+          />
+        </div>
+
+        <div className="form-group">
+          <div className="buttons">
+            <button type="submit">Continue</button>
+            <button type="button" onClick={handleReset}>
+              Reset
+            </button>
+            <button type="button" onClick={handleSaveClick}>Save</button>
+            <button type="button">Save as Template</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default IMPSPayment;
