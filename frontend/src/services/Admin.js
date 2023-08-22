e40cb9535cc4e4c28ff67b040110beb026e3d435
anshstar55 <anshul.a4571@gmail.com>
2023-08-22 15:35:25 +0530

    admin veiw added

--- frontend/src/services/Admin.js
+++ frontend/src/services/Admin.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 
-const USER_API_BASE_URL = "http://localhost:3001";
+const USER_API_BASE_URL = "http://localhost:8080";
 
 
 export function getaccounts(){
-    return axios.get(USER_API_BASE_URL + '/account');
+    return axios.get(USER_API_BASE_URL + '/veiw');
 }
