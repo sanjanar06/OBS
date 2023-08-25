f22b85004561928b4b5882ba13d15794da3ed733
anshstar55 <anshul.a4571@gmail.com>
2023-08-25 10:01:25 +0530

    validation of change pass,forget userid and styling of home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
-import '../style/Home.css';
 import { Link } from 'react-router-dom';
+import '../style/Home.css';
 
-
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="Home">
-        <header className="Home-header">
-          <h1>Welcome to Our Banking App</h1>
-          <p>Your Trusted Banking Partner</p>
-          <div className="Button-container">
+const Home = () => {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1>Welcome to Our Banking App</h1>
+        <p>Your Trusted Banking Partner</p>
+        <div className="Button-container">
           <Link to="/login">
             <button className="Button">Login</button>
-            </Link>
-            <Link to="/register">
+          </Link>
+          <Link to="/register">
             <button className="Button">Register</button>
-            </Link>
-            <Link to="/account">
+          </Link>
+          <Link to="/account">
             <button className="Button">Apply Online for Account</button>
-            </Link>
-          </div>
-        </header>
+          </Link>
+        </div>
+      </header>
+      <section className="Banking-info">
+        <div className="Banking-info-card">
+          <h2>Secure & Convenient</h2>
+          <p>Manage your finances securely and conveniently with our user-friendly online banking platform.</p>
+        </div>
+        <div className="Banking-info-card">
+          <h2>Personalized Services</h2>
+          <p>Experience personalized banking services tailored to your financial needs and goals.</p>
+        </div>
+        <div className="Banking-info-card">
+          <h2>Easy Account Application</h2>
+          <p>Applying for an account is now easier than ever. Simply fill out our online application form.</p>
+        </div>
+      </section>
+      <div className="Image-container">
+        <img src="assets/home1.jpg" alt="Banking Image" className="Banking-image" />
       </div>
-    );
-  }
-}
-export default Home
+    </div>
+  );
+};
+
+export default Home;
